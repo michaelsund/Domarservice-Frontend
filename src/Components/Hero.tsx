e1cb4e50528fb8e4d392831ca8a00fd4ed925de1
Michael Sund <michael@osund.com>
2022-07-09 20:18:32 +0200

    the 5 boxes

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -8,8 +8,7 @@ interface IProps {
 export const Hero = (props: IProps) => {
   return (
     <div
-      className={`bg-hero-pattern bg-no-repeat bg-auto bg-center
-      w-full h-96 p-6 bg-white rounded-sm border border-gray-200 shadow-md
+      className={`w-full h-96 p-6 bg-white rounded-sm border border-gray-200 shadow-md
       dark:bg-gray-800 dark:border-gray-700 ${props.className}`}
     >
       {props.children}
--- src/Containers/IndexContainer.tsx
+++ src/Containers/IndexContainer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Button } from '../Components/Button';
-import { Card } from '../Components/Card';
 import { Hero } from '../Components/Hero';
+import { ReactComponent as WhistleSvg } from '../Images/whistle.svg';
+import { ReactComponent as ClockSvg } from '../Images/clock.svg';
+import { ReactComponent as MoneySvg } from '../Images/money.svg';
+import { ReactComponent as RefereeSvg } from '../Images/referee.svg';
+import { ReactComponent as SecureSvg } from '../Images/secure.svg';
 
 const IndexContainer = () => {
   return (
     <div className="flex flex-col text-gray-900 dark:text-white">
-      <Hero className="flex content-center items-center">
-        <div className="grid w-full grid-cols-1 gap-4 place-items-center h-56">
+      <Hero className="flex content-center items-center bg-hero-pattern bg-no-repeat bg-auto bg-center">
+        <div className="grid w-full grid-cols-1 gap-4 place-items-center">
           <a href="#" className="">
             <h5 className="uppercase mb-2 text-5xl font-bold tracking-tight text-slate-50 dark:text-white">
               Hitta domare till nästa match!
@@ -22,6 +26,36 @@ const IndexContainer = () => {
           </div>
         </div>
       </Hero>
+      <Hero className="grid grid-flow-column lg:grid-cols-5 md:grid-cols-1 mt-12 p-0">
+        <div className="flex flex-col p-10 content-center items-center text-center bg-slate-50 text-black">
+          <MoneySvg className="h-10 w-10 mb-4" />
+          <h1 className="text-xl mb-2">Gratis</h1>
+          <p className="text-xs">Domarservice är helt gratis att använda.</p>
+        </div>
+        <div className="flex flex-col p-10 content-center items-center text-center bg-black text-slate-50">
+          <ClockSvg className="h-10 w-10 mb-4 fill-white" />
+          <h1 className="text-xl mb-2">Hitta domare snabbt</h1>
+          <p className="text-xs">Med domarservice hittar du domare till nästa match med rekordfart!</p>
+        </div>
+        <div className="flex flex-col p-10 content-center items-center text-center bg-slate-50 text-black">
+          <RefereeSvg className="h-10 w-10 mb-4" />
+          <h1 className="text-xl mb-2">Lokala domare</h1>
+          <p className="text-xs">Dina lokala domare på ett och samma ställe.</p>
+        </div>
+        <div className="flex flex-col p-10 content-center items-center text-center bg-black text-slate-50">
+          <WhistleSvg className="h-10 w-10 mb-4 fill-white" />
+          <h1 className="text-xl mb-2">Otroligt enkelt</h1>
+          <p className="text-xs">Du behöver bara lägga ut et match här på sidan så kommer domare att ta kontakt med dig.</p>
+        </div>
+        <div className="flex flex-col p-10 content-center items-center text-center bg-slate-50 text-black">
+          <SecureSvg className="h-10 w-10 mb-4" />
+          <h1 className="text-xl mb-2">Säker användning</h1>
+          <p className="text-xs">Domarservice har hög integritet och följer alltid GDPR.</p>
+        </div>
+      </Hero>
+      {/* <div className="mt-14 text-center w-full">
+        <h1 className="text-3xl uppercase">Kommande matcher</h1>
+      </div> */}
     </div>
   );
 };
