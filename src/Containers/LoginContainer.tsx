982ed81f317655350f858fd308cd6d51302cc32c
Michael Sund <michael@osund.com>
2022-07-27 19:11:32 +0200

    corrected production login test route

--- src/Containers/LoginContainer.tsx
+++ src/Containers/LoginContainer.tsx
@@ -22,7 +22,7 @@ const LoginContainer = () => {
     setErrorMsg('');
     axios
       .post(
-        `${process.env.NODE_ENV !== 'production' && '/api'} /authenticate/login`,
+        `${process.env.NODE_ENV === 'production' && '/api'} /authenticate/login`,
         {
           username: email,
           password,
